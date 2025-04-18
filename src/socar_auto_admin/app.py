a67c9508cd360216a41978dc6f444ed7d211c8ca
socar-sio <sio@socar.kr>
2025-04-18 16:56:59 +0900

    home 화면 추가

--- src/socar_auto_admin/app.py
+++ src/socar_auto_admin/app.py
@@ -1,22 +1,15 @@
-from flask import Flask, request, redirect, url_for, render_template_string
+from flask import Flask, request, redirect, url_for, render_template_string, render_template
 from socar_auto_admin.accident import accident_blueprint
 from socar_auto_admin.utils.automation import run_selenium_automation
 
-run_selenium_automation
-
-app = Flask(__name__)
+app = Flask(__name__, template_folder="home/templates")
 
 # 사고 등록 라우트 등록
 app.register_blueprint(accident_blueprint)
 
 @app.route("/")
 def home():
-    return '''
-        <h1>쏘카 관리자 자동화 (QA)</h1>
-        <ul>
-            <li><a href="/accident/registration">사고 자동 등록</a></li>
-        </ul>
-    '''
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
